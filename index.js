2d3e413c816186a12ade631c13704334ff2fde4c
heidiwu3388 <heidiwu3388@yahoo.com>
2022-12-13 00:06:20 -0800

    install dependencies again with appropriate versions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import all required packages
-const mysql = require("mysal2");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const chalk = require("chalk");
